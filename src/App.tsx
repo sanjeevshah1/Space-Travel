1c581d27afe3a8e13bd573319d0bdc9dd03b5e83
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-07-12 00:27:00 +0545

    added typography utilities

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ function App() {
   
 
   return (
-    <>
+    <div className='container bg-dark'>
       <h2>Hello World!</h2>
-    </>
+       <div className="" style={{padding: "1rem"}}>
+               light blue bg with a 20% alpha and white text
+       </div>
+   </div>
   )
 }
 
